1fb125ffcbcea3ee877b147cafb20e0998b88b16
Молоткова Кристина <kristinamolotkova@mail.ru>
2016-12-08 06:16:57 +0300

    изменила почти полностью задание ACL

--- acl.sh
+++ acl.sh
@@ -1,69 +1,104 @@
 #!/bin/bash
 
-rights_func ()
+choose_u_or_g()
 {
-r_val=$(echo "-")
-w_val=$(echo "-")
-x_val=$(echo "-")
-
-
-options1=("Установить право на чтение" "Установить право на запись" "Установить право на исполнение" "Выйти - все необходимые права установлены")
-select opt1 in "${options1[@]}"
-do
-    case $opt1 in
-    	"Установить право на чтение")
-    	    r_val=$(echo "r")
-	    ;;
-        "Установить право на запись")
-    	    w_val=$(echo  "w")
-    	    ;;
-        "Установить право на исполнение")
-    	    x_val=$(echo  "x")
-    	    ;;
-	"Выйти - все необходимые права установлены")
-	    break
-	    ;;
-	    	*).
-	     echo "Ошибка!"
-	    ;;
-    esac
-done
-rights=$r_val$w_val$x_val
+    while :
+    do
+	echo "Выберите пользователя или группу (u/g):"
+	read choice_u_or_g
+	if test "$choice_u_or_g" = u 
+	    then
+		echo "Вы выбрали пользователя"
+		echo "Введите имя пользователя:"
+		read name
+		grep "$name" /etc/passwd >/dev/null
+		if [ $? -ne 0 ]; then
+		    echo "Пользователь $name не существует!">&2
+		    continue
+		else
+		    break
+		fi
+	elif test "$choice_u_or_g" = g
+	    then
+		echo "Вы выбрали группу"
+		echo "Введите имя группы:"
+		read name
+		grep -q -E "^$name:" /etc/group >/dev/null
+		if [ $? -ne 0 ]; then
+		    echo "Группа $name не существует!">&2
+		    continue
+		else
+		    break
+		fi
+	else 
+	    echo "Вы ввели неверную букву" >&2
+	    continue
+	fi
+    done
 }
 
-
 file_name="$1"
-#echo "${file_name}"
-options2=("Добавить запись" "Удалить запись" "Изменить запись" "Выйти")
-select opt2 in "${options2[@]}"
+PS3="Введите номер пункта меню: "
+options=("Добавить запись" "Удалить запись" "Изменить запись" "Выйти")
+
+select opt in "${options[@]}"
 do
-    case $opt2 in
+    case "$opt" in
 	"Добавить запись")
-	    echo "Выберите пользователя или группу (u/g):"
-	    read choice
-	    if test "$choice" = u
-		then
-		    echo "Вы выбрали пользователя"
-		    echo "Введите имя пользователя:"
-		    read name_user
-		    rights_func
-		#    echo "${rights}"
-		    setfacl -m u:$name_user:$rights $file_name
-		elif test "$choice" = g
-		    then 
-			echo "Вы выбрали группу"
-			echo "Введите имя группы:"
-			read name_group
-			rights_func
-			setfacl -m g:$name_group:$rights $file_name
-		    else
-			echo "Вы ввели неверную букву"
-	    fi
+	    choose_u_or_g
+	    while :
+	    do
+		echo "Добавить право на чтение? (y/n)"
+		read answ
+		if test "$answ" = y; then
+		    setfacl -m "$choice_u_or_g:$name:r" $file_name
+		    echo "Добавлено право на чтение"
+		    break
+		elif test "$answ" = n; then
+		    break
+		else
+		    echo "Вы ввели неверную букву. Попробуйте еще раз!"
+		    continue
+		fi
+	    done
+	    while :
+	    do
+		echo "Добавить право на запись? (y/n)"
+		read answ
+		if test "$answ" = y; then
+		    setfacl -m "$choice_u_or_g:$name:w" $file_name
+		    echo "Добавлено право на запись"
+		    break
+		elif test "$answ" = n; then
+		    break
+		else
+		    echo "Вы ввели неверную букву. Попробуйте еще раз!"
+		    continue
+		fi
+	    done
+	    while :
+	    do
+		echo "Добавить право на исполнение? (y/n)"
+		read answ
+		if test "$answ" = y; then
+		    setfacl -m "$choice_u_or_g:$name:x" $file_name
+		    echo "Добавлено право на исполнение"
+		    break
+		elif test "$answ" = n; then
+		    break
+		else
+		    echo "Вы ввели неверную букву. Попробуйте еще раз!"
+		    continue
+		fi
+	    done
 	    ;;
+	    
 	"Удалить запись")
+	    choose_u_or_g
+	    setfacl -x $choice_u_or_g:$name: $file_name
+	    echo "Запись удалена"
 	    ;;
 	"Изменить запись")
-	    echo "3"
 	    ;;
 	"Выйти")
 	    break
